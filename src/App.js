f769b20e150b83fe71e18e4f0f64eb93a768fab8
Bern <bernnystrom@gmail.com>
2021-02-17 22:42:51 +0800

    Add Header component to display across different routes path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/ShopPage/ShopPage';
 import './App.css';
@@ -7,6 +8,7 @@ import './App.css';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/">
           <HomePage />
