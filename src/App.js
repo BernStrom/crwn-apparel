650c08a422126142aa6b579ef98249c7db8a36ad
Bern <bernnystrom@gmail.com>
2021-02-17 17:09:32 +0800

    Add routing to HomePage and a temporary HatsPage component for testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import './App.css';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/shop/hats">
+          <HatsPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
