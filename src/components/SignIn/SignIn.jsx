a30890914f4a72df5c73d2488686bcec22004ec9
Bern <bernnystrom@gmail.com>
2021-02-19 15:36:42 +0800

    Convert handleSubmit to async method and add email & password sign-in authentication

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import FormInput from '../FormInput/FormInput';
 import CustomButton from '../CustomButton/CustomButton';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import './SignIn.scss';
 
 class SignIn extends Component {
@@ -14,9 +14,16 @@ class SignIn extends Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: '', password: '' });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = (event) => {
@@ -34,7 +41,7 @@ class SignIn extends Component {
           <FormInput
             name="email"
             type="email"
-            label="email"
+            label="Email"
             value={this.state.email}
             handleChange={this.handleChange}
             required
@@ -42,7 +49,7 @@ class SignIn extends Component {
           <FormInput
             name="password"
             type="password"
-            label="password"
+            label="Password"
             value={this.state.password}
             handleChange={this.handleChange}
             required
