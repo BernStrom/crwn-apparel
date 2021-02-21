5f489c16b50f5e93c36bd4463ec089bcd7b20fae
Bern <bernnystrom@gmail.com>
2021-02-21 20:50:04 +0800

    Add in cart reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import userReducer from './user/userReducer';
+import cartReducer from './cart/cartReducer';
 
 export default combineReducers({
   user: userReducer,
+  cart: cartReducer,
 });
