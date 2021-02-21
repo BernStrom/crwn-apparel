b2d7850ce83d363dc58c523f51109a065e7c158f
Bern <bernnystrom@gmail.com>
2021-02-21 17:29:44 +0800

    Remove default middlewares from redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import rootReducer from './rootReducer';
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  middleware: [logger],
 });
 
 export default store;
