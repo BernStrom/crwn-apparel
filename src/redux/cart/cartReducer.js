43f85582d2ea85c291a0c641c05ea0d876d0d275
Bern <bernnystrom@gmail.com>
2021-02-21 22:16:33 +0800

    Update actions/types & reducer with cart items state and add item action

--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -1,16 +1,22 @@
 import { createAction, createReducer } from '@reduxjs/toolkit';
 import { CartActionTypes } from './cartTypes';
+import { addItemToCart } from './cart.utils';
 
 const toggleCartHidden = createAction(CartActionTypes.TOGGLE_CART_HIDDEN);
+const addItem = createAction(CartActionTypes.ADD_ITEM);
 
 const initialState = {
   hidden: true,
+  cartItems: [],
 };
 
 const cartReducer = createReducer(initialState, (builder) => {
   builder
     .addCase(toggleCartHidden, (state, action) => {
       state.hidden = !state.hidden;
+    })
+    .addCase(addItem, (state, action) => {
+      state.cartItems = addItemToCart(state.cartItems, action.payload);
     })
     .addDefaultCase((state, action) => {
       return state;
