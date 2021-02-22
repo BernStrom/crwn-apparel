f1791f11c8144e1c7ae5f65917cc2c23fa265038
Bern <bernnystrom@gmail.com>
2021-02-22 22:40:17 +0800

    Add two new cases for removing item and clearing item from cart to shopping cart reducer

--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -1,9 +1,11 @@
 import { createAction, createReducer } from '@reduxjs/toolkit';
 import { CartActionTypes } from './cartTypes';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const toggleCartHidden = createAction(CartActionTypes.TOGGLE_CART_HIDDEN);
 const addItem = createAction(CartActionTypes.ADD_ITEM);
+const removeItem = createAction(CartActionTypes.REMOVE_ITEM);
+const clearItemFromCart = createAction(CartActionTypes.CLEAR_ITEM_FROM_CART);
 
 const initialState = {
   hidden: true,
@@ -17,6 +19,12 @@ const cartReducer = createReducer(initialState, (builder) => {
     })
     .addCase(addItem, (state, action) => {
       state.cartItems = addItemToCart(state.cartItems, action.payload);
+    })
+    .addCase(removeItem, (state, action) => {
+      state.cartItems = removeItemFromCart(state.cartItems, action.payload);
+    })
+    .addCase(clearItemFromCart, (state, action) => {
+      state.cartItems = state.cartItems.filter((cartItem) => cartItem.id !== action.payload.id);
     })
     .addDefaultCase((state, action) => {
       return state;
