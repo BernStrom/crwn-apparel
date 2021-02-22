5a819e1951f1eaf00570f9d4313c791ee26a183b
Bern <bernnystrom@gmail.com>
2021-02-22 20:43:01 +0800

    Create a selector for hiding shopping cart

--- src/redux/cart/cartSelectors.js
+++ src/redux/cart/cartSelectors.js
@@ -2,6 +2,7 @@ import { createSelector } from '@reduxjs/toolkit';
 
 const selectCart = (state) => state.cart;
 
+export const selectCartHidden = createSelector([selectCart], (cart) => cart.hidden);
 export const selectCartItems = createSelector([selectCart], (cart) => cart.cartItems);
 export const selectCartItemsCount = createSelector([selectCartItems], (cartItems) =>
   cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
