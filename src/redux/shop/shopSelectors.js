a5fe17a3bb192f41292e8cd41848019149d4b94d
Bern <bernnystrom@gmail.com>
2021-02-23 23:31:48 +0800

    Add a selector for CollectionPage component and add memoization to the return of the selector func using lodash.memoize package

--- src/redux/shop/shopSelectors.js
+++ src/redux/shop/shopSelectors.js
@@ -1,5 +1,19 @@
 import { createSelector } from '@reduxjs/toolkit';
+import memoize from 'lodash.memoize';
+
+const COLLECTION_MAP_ID = {
+  hats: 1,
+  sneakers: 2,
+  jackers: 3,
+  womens: 4,
+  mens: 5,
+};
 
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector([selectShop], (shop) => shop.collections);
+export const selectCollection = memoize((collectionUrlParam) =>
+  createSelector([selectCollections], (collections) =>
+    collections.find((collection) => collection.id === COLLECTION_MAP_ID[collectionUrlParam])
+  )
+);
