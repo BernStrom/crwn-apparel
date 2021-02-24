55845715f2c3bef68e56bdbf1fb87b76b63da0ea
Bern <bernnystrom@gmail.com>
2021-02-25 00:00:00 +0800

    Add Stripe checkout button with card test message

--- src/pages/CheckoutPage/CheckoutPage.jsx
+++ src/pages/CheckoutPage/CheckoutPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import CheckoutItem from '../../components/CheckoutItem/CheckoutItem';
+import StripeCheckoutButton from '../../components/StripeCheckoutButton/StripeCheckoutButton';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cartSelectors';
 import './CheckoutPage.scss';
 
@@ -30,6 +31,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 03/23 - CWV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
