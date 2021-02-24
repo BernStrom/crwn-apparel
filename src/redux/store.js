5812f97e75fe5e255ebf8a4e916bd35ace65b52e
Bern <bernnystrom@gmail.com>
2021-02-25 02:40:25 +0800

    Add conditionals to remove redux-logger from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,15 @@ import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './rootReducer';
 
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
+
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: [logger],
+  middleware: middlewares,
 });
 
 export const persistor = persistStore(store);
