0db468740c5a85299feefb7090ac2f188573bb87
Bern <bernnystrom@gmail.com>
2021-02-26 05:40:58 +0800

    Update shop files to use redux thunk

--- src/redux/shop/shopReducer.js
+++ src/redux/shop/shopReducer.js
@@ -1,16 +1,28 @@
 import { createReducer, createAction } from '@reduxjs/toolkit';
 import ShopActionTypes from './shopTypes';
 
-const updateCollections = createAction(ShopActionTypes.UPDATE_COLLECTIONS);
+const fetchCollectionsStart = createAction(ShopActionTypes.FETCH_COLLECTIONS_START);
+const fetchCollectionsSuccess = createAction(ShopActionTypes.FETCH_COLLECTIONS_SUCCESS);
+const fetchCollectionsFailure = createAction(ShopActionTypes.FETCH_COLLECTIONS_FAILURE);
 
 const initialState = {
   collections: null,
+  isFetching: false,
+  errorMessage: undefined,
 };
 
 const shopReducer = createReducer(initialState, (builder) => {
   builder
-    .addCase(updateCollections, (state, action) => {
+    .addCase(fetchCollectionsStart, (state, action) => {
+      state.isFetching = true;
+    })
+    .addCase(fetchCollectionsSuccess, (state, action) => {
+      state.isFetching = false;
       state.collections = action.payload;
+    })
+    .addCase(fetchCollectionsFailure, (state, action) => {
+      state.isFetching = false;
+      state.errorMessage = action.payload;
     })
     .addDefaultCase((state, action) => {
       return state;
