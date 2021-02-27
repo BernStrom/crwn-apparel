b04db72ad3cf898c53d403863eb9d13f1b9e9acf
Bern <bernnystrom@gmail.com>
2021-02-27 08:22:41 +0800

    Update user reducer with sign-in/ out success and failure cases

--- src/redux/user/userReducer.js
+++ src/redux/user/userReducer.js
@@ -1,16 +1,31 @@
 import { createAction, createReducer } from '@reduxjs/toolkit';
-import { UserActionTypes } from './userTypes';
+import UserActionTypes from './userTypes';
 
-const setCurrentUser = createAction(UserActionTypes.SET_CURRENT_USER);
+const signInSuccess = createAction(UserActionTypes.SIGN_IN_SUCCESS);
+const signInFailure = createAction(UserActionTypes.SIGN_IN_FAILURE);
+const signOutSuccess = createAction(UserActionTypes.SIGN_OUT_SUCCESS);
+const signOutFailure = createAction(UserActionTypes.SIGN_OUT_FAILURE);
 
 const initialState = {
   currentUser: null,
+  error: null,
 };
 
 const userReducer = createReducer(initialState, (builder) => {
   builder
-    .addCase(setCurrentUser, (state, action) => {
+    .addCase(signInSuccess, (state, action) => {
       state.currentUser = action.payload;
+      state.error = null;
+    })
+    .addCase(signOutSuccess, (state, action) => {
+      state.currentUser = null;
+      state.error = null;
+    })
+    .addCase(signInFailure, (state, action) => {
+      state.error = action.payload;
+    })
+    .addCase(signOutFailure, (state, action) => {
+      state.error = action.payload;
     })
     .addDefaultCase((state, action) => {
       return state;
