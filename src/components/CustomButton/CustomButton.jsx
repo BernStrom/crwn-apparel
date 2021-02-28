025751504303357e58ba63114ec7af519db62299
Bern <bernnystrom@gmail.com>
2021-03-01 07:52:11 +0800

    Move all frontend react files to a dedicated client folder

--- src/components/CustomButton/CustomButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { CustomButtonContainer } from './CustomButton.styled';
-
-const CustomButton = ({ children, ...props }) => (
-  <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
-);
-
-export default CustomButton;
