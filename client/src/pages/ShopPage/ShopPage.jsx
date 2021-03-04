506d424d134951da2e69fc302307d6aa0c250389
Bern <bernnystrom@gmail.com>
2021-03-04 10:03:14 +0800

    Add lazy loading and suspense to collection containers routes to improve faster page loading

--- client/src/pages/ShopPage/ShopPage.jsx
+++ client/src/pages/ShopPage/ShopPage.jsx
@@ -1,9 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-import CollectionsOverviewContainer from '../../components/CollectionsOverview/CollectionsOverviewContainer';
-import CollectionPageContainer from '../CollectionPage/CollectionPageContainer';
+import Spinner from '../../components/Spinner/Spinner';
 import { fetchCollectionsStart } from '../../redux/shop/shopActions';
+
+const CollectionsOverviewContainer = lazy(() =>
+  import('../../components/CollectionsOverview/CollectionsOverviewContainer')
+);
+const CollectionPageContainer = lazy(() => import('../CollectionPage/CollectionPageContainer'));
 
 const ShopPage = ({ fetchCollectionsStart, match }) => {
   useEffect(() => {
@@ -12,8 +16,10 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
 
   return (
     <div className="shop-page">
-      <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
-      <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
+      <Suspense fallback={<Spinner />}>
+        <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
+      </Suspense>
     </div>
   );
 };
