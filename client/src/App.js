3ed3c0a13c589d15d9991a3661034be83aad2a7d
Bern <bernnystrom@gmail.com>
2021-03-04 10:40:34 +0800

    Wrap the error boundary component around the page routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Header from './components/Header/Header';
 import Spinner from './components/Spinner/Spinner';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import { checkUserSession } from './redux/user/userActions';
 import { selectCurrentUser } from './redux/user/userSelectors';
 import { GlobalStyle } from './global.styled';
@@ -21,20 +22,22 @@ const App = ({ checkUserSession, currentUser }) => {
     <div>
       <GlobalStyle />
       <Header />
-      <Suspense fallback={<Spinner />}>
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Route path="/shop" component={ShopPage} />
-          <Route path="/checkout">
-            <CheckoutPage />
-          </Route>
-          <Route exact path="/signin">
-            {currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />}
-          </Route>
-        </Switch>
-      </Suspense>
+      <ErrorBoundary>
+        <Suspense fallback={<Spinner />}>
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route path="/shop" component={ShopPage} />
+            <Route path="/checkout">
+              <CheckoutPage />
+            </Route>
+            <Route exact path="/signin">
+              {currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />}
+            </Route>
+          </Switch>
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 };
