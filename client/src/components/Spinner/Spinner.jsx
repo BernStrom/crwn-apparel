cbb56d44f2751f004a24cd77b69c636bf87bcb3f
Bern <bernnystrom@gmail.com>
2021-03-04 10:05:35 +0800

    Create a loading spinner component and with stylings from the WithSpinner component folder

---
+++ client/src/components/Spinner/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SpinnerOverlay, SpinnerContainer } from './Spinner.styled';
+
+const Spinner = () => (
+  <SpinnerOverlay>
+    <SpinnerContainer />
+  </SpinnerOverlay>
+);
+
+export default Spinner;
