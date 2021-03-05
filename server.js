44a238a4e7bc0567dae13f5966149d3372e8ebb7
Bern <bernnystrom@gmail.com>
2021-03-05 13:10:23 +0800

    Replace requiring external body-parser module with built-in body parsing middleware

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
 
@@ -11,8 +10,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(compression());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
