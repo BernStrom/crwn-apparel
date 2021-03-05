8aa34c04f0915fc9a8669acf032d41c9f284cb9c
Bern <bernnystrom@gmail.com>
2021-03-05 20:06:52 +0800

    Update sagas to retrieve and update cart collection in firebase based on the logged-in user id

--- client/src/redux/cart/cartSagas.js
+++ client/src/redux/cart/cartSagas.js
@@ -1,15 +1,49 @@
-import { all, call, put, takeLatest } from 'redux-saga/effects';
+import { all, call, put, takeLatest, select } from 'redux-saga/effects';
+import { getUserCartRef } from '../../firebase/firebase.utils';
 import UserActionTypes from '../user/userTypes';
-import { clearCart } from './cartActions';
+import { selectCurrentUser } from '../user/userSelectors';
+import CartActionTypes from './cartTypes';
+import { clearCart, setCartFromFirebase } from './cartActions';
+import { selectCartItems } from './cartSelectors';
 
 export function* clearCartOnSignOut() {
   yield put(clearCart());
+}
+
+export function* updateCartInFirebase() {
+  const currentUser = yield select(selectCurrentUser);
+  if (currentUser) {
+    try {
+      const cartRef = yield getUserCartRef(currentUser.id);
+      const cartItems = yield select(selectCartItems);
+      yield cartRef.update({ cartItems });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+export function* checkCartFromFirebase({ payload: user }) {
+  const cartRef = yield getUserCartRef(user.id);
+  const cartSnapshot = yield cartRef.get();
+  yield put(setCartFromFirebase(cartSnapshot.data().cartItems));
 }
 
 export function* onSignOutSuccess() {
   yield takeLatest(UserActionTypes.SIGN_OUT_SUCCESS, clearCartOnSignOut);
 }
 
+export function* onUserSignIn() {
+  yield takeLatest(UserActionTypes.SIGN_IN_SUCCESS, checkCartFromFirebase);
+}
+
+export function* onCartChange() {
+  yield takeLatest(
+    [CartActionTypes.ADD_ITEM, CartActionTypes.REMOVE_ITEM, CartActionTypes.CLEAR_ITEM_FROM_CART],
+    updateCartInFirebase
+  );
+}
+
 export function* cartSagas() {
-  yield all([call(onSignOutSuccess)]);
+  yield all([call(onSignOutSuccess), call(onCartChange), call(onUserSignIn)]);
 }
